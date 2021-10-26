2acdf354de149c96cab34b8b110509c1b1d976ac
sundaresan17feb@gmail.com <sundaresan17feb@gmail.com>
2021-10-26 20:02:07 +0530

    First commit of proj01

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ request.open('GET','https://restcountries.com/v3.1/all',true);
 request.send();
 request.onload = function(){var data= JSON.parse(request.response);
                   
-                  // console.log(data);}
+                  console.log(data);}
                   //console.log(data[0].region);}
                   //for(var i=0;i<data.length;i++)
                   //console.log(data[i].region);}
@@ -11,6 +11,6 @@ request.onload = function(){var data= JSON.parse(request.response);
                   //console.log(data[i].capital);}
                   //for(var i=0;i<data.length;i++)
                   //console.log(data[i].name);} res    = data.filter ( (data)  => data.population<1000000);
-   var res= data.filter((ele)=>ele.population<1000000);
+   //var res= data.filter((ele)=>ele.population<1000000);
  
-                  console.log(res);
+                 // console.log(res);
